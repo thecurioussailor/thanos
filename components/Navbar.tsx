8a0b1b9bae163016dc96d3f146847c51ac8f294f
Ashutosh Sagar <48312681+thecurioussailor@users.noreply.github.com>
2024-09-01 23:44:44 +0530

    git updated

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import { Switch } from './ui/switch'
 import Image from 'next/image'
 import Logo from '../public/logo-thonos2.png'
 import { FaGithub } from 'react-icons/fa'
 import { RxAvatar } from 'react-icons/rx'
 const Navbar = () => {
+  const [isDarkMode, setIsDarkMode] = useState(true);
   return (
     <div className='flex justify-center text-[#fdfdfd] h-[64px]'>
         <div className='flex flex-row justify-between items-center px-4 w-full'>
@@ -21,11 +23,19 @@ const Navbar = () => {
             </div>
             <div className='text-white flex justify-center items-center'>
               <div className='mr-4 flex'>
-                    <Switch className='bg-white'/>
+                <div>
+                  <button onClick={() => {
+                    setIsDarkMode(!isDarkMode)
+                  }}>
+                      <Switch 
+                        checked={isDarkMode}
+                      />
+                  </button>
+                </div>
               </div>
               <div className='flex mr-2'>
-                <a href=''><FaGithub className='mr-2' size={20}/></a>
-                <RxAvatar size={22}/>
+                <a target='_blank' href='https://github.com/thecurioussailor'><FaGithub className='mr-2' size={20}/></a>
+                <a target='_blank' href='https://x.com/sagar11ashutosh'><RxAvatar size={22}/></a>
               </div>
             </div>
         </div>
